29d4335ed80e6748d30c057e3e3fba4d4387dbbb
taemin-steve <okchrisok@naver.com>
2021-11-12 00:42:50 +0900

    Map practice

--- GustoMap/app/src/main/java/com/example/gustomap/MainActivity.java
+++ GustoMap/app/src/main/java/com/example/gustomap/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.gustomap;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.app.FragmentManager;
 import android.os.Bundle;
 
 import android.view.MenuItem;
@@ -9,9 +12,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.PopupMenu;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
     Button region;
     
 
@@ -19,7 +31,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //setContentView(R.layout.map);
         region = (Button) findViewById(R.id.region);
+
+//        fragmentManager = getFragmentManager();
+//        mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.googleMap);
+//        mapFragment.getMapAsync(this);
+
     }
 
 
@@ -48,5 +66,17 @@ public class MainActivity extends AppCompatActivity {
                 }
         );
         popup.show();
+    }
+
+    @Override
+    public void onMapReady(@NonNull GoogleMap googleMap) { //마커를 꼽는 것.
+//        LatLng location = new LatLng(37.49128301811198, 126.9188174758624);
+//        MarkerOptions markerOptions = new MarkerOptions();
+//        markerOptions.title("보라매 공원");
+//        markerOptions.snippet("공원");
+//        markerOptions.position(location);
+//        googleMap.addMarker(markerOptions);
+//
+//        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 16));
     }
 }
